aef025249a727c1eb4d67ddc15716cd9b4de8752
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-21 17:01:05 -0700

    Added onClick events to buttons; MVP complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const TOUCHDOWN = 7;
+  const FIELD_GOAL =3
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   
@@ -30,12 +32,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => {setHomeScore(homeScore+TOUCHDOWN)}}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => {setHomeScore(homeScore+FIELD_GOAL)}}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => {setAwayScore(awayScore+TOUCHDOWN)}}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {setAwayScore(awayScore+FIELD_GOAL)}}>Away Field Goal</button>
         </div>
       </section>
     </div>
